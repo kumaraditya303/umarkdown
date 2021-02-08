3663f06a3325b438c7bd21c1f17664d96cb574dc
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-02-08 14:29:22 +0530

    build: Build with pep517

--- scripts/manylinux.sh
+++ scripts/manylinux.sh
@@ -4,10 +4,8 @@ mkdir -p /tmp/temp-wheels
 find /tmp/temp-wheels/ -type f -delete
 
 for PYBIN in /opt/python/cp3[789]*/bin; do
-    "${PYBIN}/pip" install -q -U setuptools wheel --no-cache-dir
-    (cd /io/ && "${PYBIN}/python" -m pip install -r requirements.txt)
-    (cd /io/ && "${PYBIN}/python" -m pip install .)
-    (cd /io/ && "${PYBIN}/python" setup.py -q bdist_wheel -d /tmp/temp-wheels)
+    "${PYBIN}/pip" install -q build --no-cache-dir
+    (cd /io/ && "${PYBIN}/python" -m build --wheel -o /tmp/temp-wheels)
 done
 
 "$PYBIN/pip" install -q auditwheel
