b22964c3473c2d0d0addf1fe75c5e2abfcdd57cf
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-12-17 09:54:39 +0000

    dev

--- umarkdown/_internal.pyi
+++ umarkdown/_internal.pyi
@@ -12,35 +12,3 @@ def markdown(
     unsafe: Optional[bool] = ...,
     validate_utf8: Optional[bool] = ...,
 ) -> str: ...
-@overload
-def markdown(
-    text_file: str,
-    source_pos: Optional[bool] = ...,
-    hard_breaks: Optional[bool] = ...,
-    no_breaks: Optional[bool] = ...,
-    smart: Optional[bool] = ...,
-    unsafe: Optional[bool] = ...,
-    validate_utf8: Optional[bool] = ...,
-) -> str: ...
-@overload
-def markdown(
-    text: str,
-    output_file: str,
-    source_pos: Optional[bool] = ...,
-    hard_breaks: Optional[bool] = ...,
-    no_breaks: Optional[bool] = ...,
-    smart: Optional[bool] = ...,
-    unsafe: Optional[bool] = ...,
-    validate_utf8: Optional[bool] = ...,
-) -> bool: ...
-@overload
-def markdown(
-    text_file: str,
-    output_file: str,
-    source_pos: Optional[bool] = ...,
-    hard_breaks: Optional[bool] = ...,
-    no_breaks: Optional[bool] = ...,
-    smart: Optional[bool] = ...,
-    unsafe: Optional[bool] = ...,
-    validate_utf8: Optional[bool] = ...,
-) -> bool: ...
